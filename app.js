010a43a121a4bd7d0d27110c3725a1a0523b36ad
AmrAshraf21 <amrashraf314@gmail.com>
2022-11-07 20:47:34 +0200

    Testing from first endpoint

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+app.listen(5000);
