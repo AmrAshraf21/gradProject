d6fe653b5d8c057481436a532825499f4e0d0fc1
MostafaMGomaa <gomaamostafa26@gmail.com>
2023-06-27 17:04:52 +0300

    fix removeFromFavorits()

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,6 +1,6 @@
 exports.errorHandler = (error, req, res, next) => {
-	console.log(error.data);
-	console.log(error.message);
+	// console.log(error.data);
+	console.log(error);
 	const status = error.statusCode || 500;
 	const message = error.message;
 	const data = error.data;
--- models/user.js
+++ models/user.js
@@ -1,97 +1,69 @@
 const Book = require('./book');
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type:String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-	image: {
-		type: String
-	 },
-  role: {
-		type: String,
-		default: 'user',
-	},
-  
-  resetToken: {
-    type: String
-  },
-  resetTokenExpiration: {
-    type: Date
-  },
+const userSchema = new Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		image: {
+			type: String,
+		},
+		role: {
+			type: String,
+			default: 'user',
+		},
+		resetToken: {
+			type: String,
+		},
+		resetTokenExpiration: {
+			type: Date,
+		},
 
-  wishlist: {
-    books: [
-      {
-        type: mongoose.Types.ObjectId,
-				ref: 'Book'
-			},
-		],
+		wishlist: {
+			books: [
+				{
+					type: mongoose.Types.ObjectId,
+					ref: 'Book',
+				},
+			],
+		},
+		alreadyRead: {
+			books: [
+				{
+					type: mongoose.Types.ObjectId,
+					ref: 'Book',
+				},
+			],
+		},
+		favorits: {
+			books: [
+				{
+					book_item: { type: mongoose.Types.ObjectId, ref: 'Book' },
+					is_read: { type: Number, default: 0 },
+					rating: { type: Number, default: 0 },
+					is_reviewed: { type: Number, default: 0 },
+				},
+			],
+		},
 	},
-  alreadyRead: {
-    books: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: 'Book',
-      },
-    ],
-  },
-  favorits: {
-    books: [
-      {
-        book_item: { type: mongoose.Types.ObjectId, ref: 'Book' },
-        is_read: { type: Number, default: 0 },
-        rating: { type: Number, default: 0 },
-        is_reviewed: { type: Number, default: 0 }
-      },
-    ],
-  }
-  //   favorits: {
-  //   books: [
-  //     {
-  //       type: mongoose.Types.ObjectId,
-  //       ref: 'Book',
-  //     },
-  //   ],
-  //   is_read: { type: Number, default: 0 },
-  //   rating: { type: Number, default: 0 },
-  //   is_reviewed: { type: Number, default: 0 }
-  // },
-  // favorits: {
-  //   books: [
-  //     {
-  //       type: mongoose.Types.ObjectId, ref: 'Book',
-  //       is_read: { type: Number, default: 0 },
-  //       rating: { type: Number, default: 0 },
-  //       is_reviewed: { type: Number, default: 0 }
-  //     },
-  //   ],
-  // }
-}, { timestamps: true });
-
-// favorits: [
-//       {
-//         book: { type: mongoose.Types.ObjectId, ref: 'Book' },
-//         is_read: { type: Number, default: 0 },
-//         rating: { type: Number, default: 0 },
-//         is_reviewed: { type: Number, default: 0 }
-//       }
-//   ],
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('User', userSchema);
